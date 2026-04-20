84af31d867c773f9b133e001db9dbaa735238fd1
KongNontawatDev <kns.dev15885@gmail.com>
2026-04-20 14:33:50 +0700

    run build

--- dist/assets/mobile-content-DAeAcM7i.js
+++
@@ -1,6 +0,0 @@
-import{c as t,j as a,f as r}from"./index-J22NwmdT.js";/**
- * @license lucide-react v0.542.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const i=[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]],o=t("house",i);function l({children:e,className:s,showSafeArea:n=!0}){return a.jsx("div",{className:r("min-h-screen bg-white dark:bg-gray-900",s),children:a.jsx("div",{className:"mx-auto max-w-lg min-h-screen",children:a.jsx("div",{className:r("min-h-screen bg-white dark:bg-gray-900",n&&"mobile-safe-area"),children:e})})})}function m({children:e,className:s,padding:n=!0}){return a.jsx("main",{className:r("flex-1",n&&"p-4",s),children:e})}export{o as H,l as M,m as a};
